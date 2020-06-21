8b4d4b9864fa54d5539c5a1fab769347a2c412f4
Nour <Nour7011@gmail>
2020-06-21 23:50:25 +0200

    small area mask color

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -1018,14 +1018,22 @@ class Visualizer:
         #     has_valid_segment = True
         #     self.output.ax.imshow(rgba)
       
-        # draw polygons for regular masks
-        for segment in mask.polygons:
-            area = mask_util.area(mask_util.frPyObjects([segment], shape2d[0], shape2d[1]))
-            if area < (area_threshold or 0):
-                continue
+       if not mask.has_holes:
+            # draw polygons for regular masks
+            for segment in mask.polygons:
+                area = mask_util.area(mask_util.frPyObjects([segment], shape2d[0], shape2d[1]))
+                has_valid_segment = True
+                segment = segment.reshape(-1, 2)
+                if area < (area_threshold or 0):
+                    self.draw_polygon(segment, color=[0, 0, 0], edge_color=edge_color, alpha=alpha)
+                self.draw_polygon(segment, color=color, edge_color=edge_color, alpha=alpha)
+
+        else:
+            rgba = np.zeros(shape2d + (4,), dtype="float32")
+            rgba[:, :, :3] = color
+            rgba[:, :, 3] = (mask.mask == 1).astype("float32") * alpha
             has_valid_segment = True
-            segment = segment.reshape(-1, 2)
-            self.draw_polygon(segment, color=color, edge_color=edge_color, alpha=alpha)
+            self.output.ax.imshow(rgba)
     
 
         if text is not None and has_valid_segment:
