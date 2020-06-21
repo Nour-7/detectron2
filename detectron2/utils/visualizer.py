c5365a7b4a160921b07e6c2bf3fcf114fa710c15
Nour <Nour7011@gmail>
2020-06-22 00:54:26 +0200

    Regular mask edit

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -430,7 +430,7 @@ class Visualizer:
             self.draw_binary_mask(
                 binary_mask,
                 color=mask_color,
-                edge_color=_OFF_WHITE,
+                edge_color=None,
                 text=None,
                 alpha=alpha,
                 area_threshold=area_threshold,
