bf3d07467bed7ec168d0dfe46e56cfab25d59fa6
Nour <Nour7011@gmail>
2020-07-02 01:25:32 +0200

    change the default value of  area_threshold and alpha

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -401,7 +401,7 @@ class Visualizer:
         )
         return self.output
 
-    def draw_sem_seg(self, sem_seg, area_threshold=None, alpha=0.8):
+    def draw_sem_seg(self, sem_seg, area_threshold=1500, alpha=1.0):
         """
         Draw semantic segmentation predictions/labels.
 
@@ -1017,24 +1017,7 @@ class Visualizer:
             rgba[:, :, :3] = color
             rgba[:, :, 3] = (mask.mask == 1).astype("float32") * alpha
             has_valid_segment = True
-            self.output.ax.imshow(rgba)
-      
-        # if not mask.has_holes:
-        #      # draw polygons for regular masks
-        #     for segment in mask.polygons:
-        #         area = mask_util.area(mask_util.frPyObjects([segment], shape2d[0], shape2d[1]))
-        #         has_valid_segment = True
-        #         segment = segment.reshape(-1, 2)
-        #         if area < (area_threshold or 0):
-        #             self.draw_polygon(segment, color=[0, 0, 0], edge_color=edge_color, alpha=alpha)
-        #         self.draw_polygon(segment, color=color, edge_color=edge_color, alpha=alpha)
-        # else:
-        #     rgba = np.zeros(shape2d + (4,), dtype="float32")
-        #     rgba[:, :, :3] = color
-        #     rgba[:, :, 3] = (mask.mask == 1).astype("float32") * alpha
-        #     has_valid_segment = True
-        #     self.output.ax.imshow(rgba)
-    
+            self.output.ax.imshow(rgba)    
 
         if text is not None and has_valid_segment:
             # TODO sometimes drawn on wrong objects. the heuristics here can improve.
